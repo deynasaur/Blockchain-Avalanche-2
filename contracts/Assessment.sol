2a0c9b48f2d99f0a142d9fb610d79840eb10c95c
z <144504951+deynasaur@users.noreply.github.com>
2024-12-02 17:17:04 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,46 +1,69 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
+
     address payable public owner;
     uint256 public balance;
 
+    struct Racket {
+        string model;
+        uint cost;
+    }
+
+    mapping(string => Racket) private store;
+    mapping(address => string[]) private myRackets;
+
     event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Withdraw(uint256 _withdrawAmount);
+    event RacketBought(uint amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+
+        store["Yonex Astrox 77 Pro"] = Racket("Yonex Astrox 77 Pro", 1);
+        store["RSL Magnum M8"] = Racket("RSL Magnum M8", 1);
+        store["Victor DriveX KT I"] = Racket("Victor DriveX KT I", 1);
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
+    modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
 
-        // perform transaction
+    function showStore() public view returns (Racket[] memory) {
+        Racket[] memory rckt = new Racket[](3);
+        rckt[0] = store["Yonex Astrox 77 Pro"];
+        rckt[1] = store["RSL Magnum M8"];
+        rckt[2] = store["Victor DriveX KT I"];
+        return rckt;
+    }
+
+    function buyRacket(string memory racketID) public payable {
+        Racket memory rckt = store[racketID];
+
+        require(bytes(rckt.model).length != 0, "Racket does not exist.");
+        require(msg.value >= rckt.cost * 1 ether, "Insufficient Ether Balance.");
+
+        balance -= rckt.cost;
+        myRackets[msg.sender].push(racketID);
+        emit RacketBought(rckt.cost);
+    }
+
+    function deposit(uint256 _amount) public payable onlyOwner {
+        uint _previousBalance = balance;
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
+
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function withdraw(uint256 _withdrawAmount) public payable onlyOwner {
         uint _previousBalance = balance;
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -48,13 +71,17 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
     }
+
+    function showMyRackets() public view returns (string[] memory) {
+        return myRackets[msg.sender];
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
 }
